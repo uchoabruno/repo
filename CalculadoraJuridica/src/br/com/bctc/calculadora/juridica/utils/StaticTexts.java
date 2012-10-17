0dba46fd1b32a8fd4b3525d95f96e504eb1896a5
uchoabruno <uchoabruno@yahoo.com.br>
2012-10-17 09:55:04 -0300

    Inclusão dos textos estáticos

---
+++ CalculadoraJuridica/src/br/com/bctc/calculadora/juridica/utils/StaticTexts.java
@@ -0,0 +1,11 @@
+package br.com.bctc.calculadora.juridica.utils;
+
+/**
+ * Classe que possui os textos de exibição. Posteriormente, passar para um arquivo de properties.
+ */
+public class StaticTexts {
+
+	//public static final String  = "";
+	public static final String TOOL_TIP_TEXT_VALOR_CAUSA = "Valor original da causa.";
+
+}
